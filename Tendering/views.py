d5d652997b5fcfd22bba2aff63593324dae1b544
unknown <alloushmhyar@gmail.com>
2026-06-12 16:25:15 +0300

    Exclude user's own tenders from homepage

--- Tendering/views.py
+++ Tendering/views.py
@@ -141,10 +141,17 @@ class TenderViewSet(viewsets.ModelViewSet):
             return queryset.select_related('category', 'currency', 'location', 'status').prefetch_related('attachments')
             
         return queryset.filter(
-            is_approved=True,
-            deadline__gt=timezone.now()
-        ).exclude(status__name__in=["Awarded", "Closed"]).select_related('category', 'currency', 'location', 'status').prefetch_related('attachments')
-
+    is_approved=True,
+    deadline__gt=timezone.now()
+        ).exclude(
+            user=user
+        ).exclude(
+            status__name__in=["Awarded", "Closed"]
+        ).select_related(
+            'category', 'currency', 'location', 'status'
+        ).prefetch_related('attachments')
+    
+    
     @action(detail=True, methods=['post'], permission_classes=[IsAdminUser])
     def approve(self, request, pk=None):
         tender = self.get_object()
