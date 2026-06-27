a94c5c4885687f02283b6fbb28eb5bae3f84bcc0
Henry <odaidhiab@gmail.com>
2026-06-27 12:14:33 +0300

    Merge branch 'main' of https://github.com/OdaiDhiab/TenderApplicationBackend

--- Tendering/views.py
+++ Tendering/views.py
@@ -36,12 +36,8 @@ class RegisterView(APIView):
         if serializer.is_valid():
             user = serializer.save()
 
-            refresh = RefreshToken.for_user(user)
-
             return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                'user': UserSerializer(user).data
+                "message": "User registered successfully, wait for admin approval",
             }, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
